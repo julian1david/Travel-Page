7a670accb45c815cfbc8cef8d8fbc43646939096
Julian F <juliandavidbf@gmail.com>
2023-01-02 23:55:58 -0500

    [UPDATE] package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,39 @@
-const colors = require('tailwindcss/colors');
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-  content: ['./public/**/*.{html,js}'],
-  darkMode: 'class',
+  content: ["./index.html", "./src/**/*.{html,js}", "./public/**/*.{html,js}"],
+  darkMode: "class",
   theme: {
     colors: {
-      'primary':"#CC2D4A",
-      'secondary':"#8FA206",
-      'tertiary':"#61AEC9",
+      primary: "#CC2D4A",
+      secondary: "#8FA206",
+      tertiary: "#61AEC9",
       black: colors.black,
       white: colors.white,
       gray: colors.gray,
     },
-    fontFamily: { 
+    fontFamily: {
       Montserrat: ["Monserrat", "sans-serif"],
     },
     extend: {
       backgroundImage: {
-        'sanFrancisco': "url('../img/sanFrancisco.jpg')",
-			  'sanFranciscoDesktop': "url('../img/sanFranciscoDesktop.jpg')",
-			  'yosemite': "url('../img/yosemite.jpg')",
-			  'LA': "url('../img/LA.jpg')",
-			  'seattle': "url('../img/seattle.jpg')",
-			  'new_york': "url('../img/new_york.jpg')",
-			  'norway': "url('../img/norway.jpg')",
-			  'sydney': "url('../img/sydney.jpg')",
-			  'miami': "url('../img/miami.jpg')",
-			  'switzerland': "url('../img/switzerland.jpg')",
-			  'bali': "url('../img/bali.jpg')",
-			  'chicago': "url('../img/chicago.jpg')",
-			  'europe': "url('../img/europe.jpg')",
-			  'iceland': "url('../img/iceland.jpg')",
-			  'icelandCard': "url('../img/cards-mini/iceland.jpg')",
+        sanFrancisco: "url('../img/sanFrancisco.jpg')",
+        sanFranciscoDesktop: "url('../img/sanFranciscoDesktop.jpg')",
+        yosemite: "url('../img/yosemite.jpg')",
+        LA: "url('../img/LA.jpg')",
+        seattle: "url('../img/seattle.jpg')",
+        new_york: "url('../img/new_york.jpg')",
+        norway: "url('../img/norway.jpg')",
+        sydney: "url('../img/sydney.jpg')",
+        miami: "url('../img/miami.jpg')",
+        switzerland: "url('../img/switzerland.jpg')",
+        bali: "url('../img/bali.jpg')",
+        chicago: "url('../img/chicago.jpg')",
+        europe: "url('../img/europe.jpg')",
+        iceland: "url('../img/iceland.jpg')",
+        icelandCard: "url('../img/cards-mini/iceland.jpg')",
       },
     },
   },
-  plugins: [
-    require('tailwind-scrollbar-hide')
-  ],
+  plugins: [require("tailwind-scrollbar-hide")],
 };
